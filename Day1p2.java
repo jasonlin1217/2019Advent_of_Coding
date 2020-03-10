5488b1fdd94d3558b84961e6c27893b46fb036da
jasonlin1217 <jasonlin509@gmail.com>
2020-03-10 16:53:39 -0400

    adding day1p2 code

---
+++ Day1p2.java
@@ -0,0 +1,43 @@
+import java.io.*;
+import java.util.* ; 
+
+public class Day1p1 {
+	/*
+	Not sure what proper input is so I'm just gonna use
+	an arraylist of modules.
+	*/
+
+	private static int fuelCalc(int mass){
+        int value = mass/3 - 2;
+        if (value <= 0) {
+            return 0;
+        }
+        return value;
+	}
+
+    private static Integer totalFuel(List<Integer> modules){
+		int sum = 0;
+		for (int i = 0; i < modules.size(); i++) {
+            int x = modules.get(i);
+            while (fuelCalc(x) != 0) {
+                sum = sum + fuelCalc(x);
+                x = fuelCalc(x);
+            }
+		}
+		return sum;
+
+	}
+
+	public static void main(String[] args) throws FileNotFoundException {
+		List<Integer> iFile = new ArrayList<>();
+		File input = new File("day1input.txt");
+		Scanner sc = new Scanner(input);
+		while(sc.hasNext()){
+			iFile.add(sc.nextInt());
+		}
+		System.out.println(totalFuel(iFile));
+	}
+
+}
+
+
